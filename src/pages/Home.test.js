68ee72c645423b47be038691dbf60b78668de04d
Anthony Hinojosa <anthonyhinojosa@adminisatorsmbp.mynetworksettings.com>
2023-07-31 17:16:49 -0400

    New Attempt 4

--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Home from './Home';
+import Trainers from '../components/Trainers/Trainers';
 
 describe('Home component', ()=>{
     test('renders Home Comp', ()=>{
@@ -17,8 +18,11 @@ describe('Home component', ()=>{
                 <Home/>
             </MemoryRouter>
         );
-        const heading = getByText("El Tigre's Gear");
-        expect(heading).toBeInTheDocument();
+        // const heading = getByText("El Tigre's Gear");
+        // const heading = screen.getByRole('heading', { name: /El Tigre's Gear/i });
+        // expect(heading).toHaveTextContent("El Tigre's Gear");
+        let wrapper = document.querySelector('h1');
+expect(wrapper?.textContent).toEqual("El Tigre's Gear");
     });
     test('renders the button', ()=>{
         const { getByText } = render(
@@ -26,16 +30,17 @@ describe('Home component', ()=>{
                 <Home/>
             </MemoryRouter>
         );
-        const orderButton = getByText(/ORDER NOW/i);
-        expect(orderButton).toBeInTheDocument();
+        // const orderButton = getByText("ORDER NOW");
+        let wrapperbttn = document.querySelector('button');
+        expect(wrapperbttn.textContent).toEqual("ORDER NOW");
     });
-    test('renders the Trainers Comp', ()=>{
+    test('renders the Trainers Component', ()=>{
         const { getByTestId } = render(
             <MemoryRouter>
                 <Home/>
             </MemoryRouter>
         );
-        const trainers = getByTestId('trainers');
+        const trainers = getByTestId(<Trainers/>);
         expect(trainers).toBeInTheDocument();
     });
 })
